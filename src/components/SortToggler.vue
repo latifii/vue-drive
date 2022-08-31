6968d5eb7aa75001896da27d3adcc781d7bf77e3
latifii <latifii68@gmail.com>
2022-08-31 22:17:07 +0430

    add sort component

---
+++ src/components/SortToggler.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="toggle" class="rounded-button">
+    <icon-arrow-up v-if="sort.order === 'asc'" />
+    <icon-arrow-down v-else />
+  </button>
+</template>
+
+<script setup>
+import { reactive, defineEmits } from 'vue';
+
+const sort = reactive({
+  column: 'name',
+  order: 'asc',
+});
+
+const emit = defineEmits(['sort-change']);
+
+const toggle = () => {
+  sort.order = sort.order === 'asc' ? 'desc' : 'asc';
+  emit('sort-change', sort);
+};
+</script>
--- src/pages/MyFiles.vue
+++ src/pages/MyFiles.vue
@@ -4,26 +4,49 @@
 
     <div class="d-flex justify-content-between align-items-center py-2">
       <h6 class="text-muted mb-0">Files</h6>
-      <button class="rounded-button">
-        <icon-arrow-up />
-      </button>
+      <sort-toggler @sort-change="handleSortChange" />
     </div>
     <files-list :files="files" />
   </div>
 </template>
 
-<script setup>
+<script>
 import ActionBar from '../components/ActionBar.vue';
+import SortToggler from '../components/SortToggler.vue';
 import FilesList from '../components/files/FilesList.vue';
 import filesApi from '../api/files';
-import { onMounted, ref } from 'vue';
+import { reactive, ref, watchEffect } from 'vue';
 
-const files = ref([]);
-const fetchFiles = () => {
-  filesApi
-    .index()
-    .then((res) => (files.value = res.data))
-    .catch((err) => console.log(err.message));
+const fetchFiles = async (query) => {
+  try {
+    const { data } = await filesApi.index(query);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
-onMounted(() => fetchFiles());
+
+export default {
+  components: { ActionBar, SortToggler, FilesList },
+  setup() {
+    const files = ref([]);
+    const query = reactive({
+      _sort: 'name',
+      _order: 'asc',
+    });
+    const handleSortChange = (payload) => {
+      query._sort = payload.column;
+      query._order = payload.order;
+    };
+
+    //   await filesApi
+    //     .index(query)
+    //     .then((res) => (files.value = res.data))
+    //     .catch((err) => console.log(err.message));
+    // };
+    watchEffect(async () => (files.value = await fetchFiles(query)));
+
+    return { files, handleSortChange };
+  },
+};
 </script>
