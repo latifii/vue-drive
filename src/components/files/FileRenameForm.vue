c8862578bca32381a2109c04981ee2ac54c5fe60
latifii <latifii68@gmail.com>
2022-09-05 20:21:59 +0430

    create file rename

---
+++ src/components/files/FileRenameForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <input type="text" class="form-control" />
+    <div class="d-flex flex-row-reverse mt-3">
+      <button class="btn btn-primary">OK</button>
+      <button class="btn btn-outline-secondary me-2">Cansel</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
